54a70115a6242981fd13cf5d8a31baabe005a58c
anjali-kalam08 <anjalikalam16@gmail.com>
2026-01-14 22:34:41 +0530

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const hamb = document.querySelector('.header .nav-bar .nav-list .hamb');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list .ul');
-const header = document.querySelector('.header .container');
+const header = document.querySelector('.header.container');
 
-humb.addEventListener('click', () => {
-    humb.classList.toggle('active');
+hamb.addEventListener('click', () => {
+    hamb.classList.toggle('active');
     mobile_menu.classList.toggle('active');
 });
 
-document.addEventListener('scroll', () => {
+document.addEventListener('scroll' () => {
     var scroll_position = window.scrollY;
     if(scroll_position > 250){
-        header.style.backgroundColor = '#215189ff';
-    }else{
-        header.style.backgroundColor = 'transparent';
-    }
-})
+        header.computedStyleMap.backgroundcolor = '#29323c';
+    }else(
+        header.computedStyleMap.backgroundcolor = 'transparent';
+    )
+});
